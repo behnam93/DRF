21f77a363b78dc3ba5ee657dbfe603c4a176eda8
Behnam <behnams_id@yahoo.com>
2020-03-19 13:59:31 +0330

    add calculator api

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    std_number = models.IntegerField()
+
+    TEACHER = (
+        ("razavizadeh", "Razavizadeh"),
+        ("vakili", "Vakili"),
+        ("beheshti", "Beheshti")
+    )
+    teacher = models.CharField(max_length=12, choices=TEACHER)
